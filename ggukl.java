af19fdd21371a61cc1b9d2d8ecb46bdd452d5b72
radistykda <radishkda@gmail.com>
2025-11-03 14:34:24 +0700

    Add files via upload

---
+++ ggukl.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class ggukl {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Masukan angka: ");
+        int angka = input.nextInt();
+
+        if (angka % 2 == 0) {
+            System.out.println(angka + " adalah bilangan genap");
+        } else {
+            System.out.println(angka + " adalah bilangan ganjil");
+        }
+         input.close();
+    }
+}
